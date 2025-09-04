c71d1ef4b357797ae8fbe9f8ad8c1e84a3a123f0
ouusagi <xmflzhqxj123@naver.com>
2025-09-04 21:14:48 +0900

    MOODÉA - 4개국어 번역 감지 위젯 추가

--- src/MainPage_components/TopMenu.jsx
+++ src/MainPage_components/TopMenu.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import './MainPage.css';
+import { useEffect } from "react";
 
 function TopMenu(){
+
+    useEffect(()=>{
+        window.gtranslateSettings = {
+      default_language: "ko",
+      native_language_names: true,
+      detect_browser_language: true,
+      languages: ["ko", "ja", "en", "zh-CN"],
+      wrapper_selector: ".gtranslate_wrapper",
+      flag_size: 24,
+      alt_flags: { en: "usa" }
+    };
+
+    if(!document.querySelector('script[src="https://cdn.gtranslate.net/widgets/latest/flags.js"]')){
+        const script = document.createElement("script")
+        script.src = "https://cdn.gtranslate.net/widgets/latest/flags.js"
+        script.defer = true
+        document.body.appendChild(script)
+    }
+    },[])
+
 
     return(
 
 
         <div>
-            {/* 상단 옵션창 */}
+
     <div className="menu-container">
 
         <div className="option-container">
@@ -19,20 +40,19 @@ function TopMenu(){
         </div>
 
         <div className="menuicon-container">
+        <div class="gtranslate_wrapper"></div>
         <a href="#"><i className="fa-regular fa-user"></i></a>
         <a href="#"><i className="fa-solid fa-basket-shopping"></i></a>
         <a href="#"><i className="fa-solid fa-magnifying-glass"></i></a>
         </div>
 
     </div>
-    {/* 상단 옵션창 */}
+
         </div>
 
     )
 
 }
-
-
 
 
 
