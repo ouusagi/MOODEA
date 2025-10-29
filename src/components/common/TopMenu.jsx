15e4be4cec62966f95f42053783252b96bef29ef
ouusagi <xmflzhqxj123@naver.com>
2025-10-29 16:35:11 +0900

    MOODÉA - 로그아웃 기능 구현

--- src/components/common/TopMenu.jsx
+++ src/components/common/TopMenu.jsx
@@ -1,8 +1,26 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import supabase from "../../supabaseClient";
 
 function TopMenu(){
+
+    const [user,setuser] = useState(null)
+
+    useEffect(()=>{
+        const checkSession = async ()=>{
+            const {data, error} = await supabase.auth.getSession()
+            if(error){console.log(error.message)}
+            else{setuser(data.session.user)}
+        }
+        checkSession()
+    },[])
+    
+    const handleLogout = async ()=>{
+        const {error} = await supabase.auth.signOut()
+        if(error){console.log(error.message)}
+        else{setuser(null)}
+    }
 
     useEffect(()=>{
         window.gtranslateSettings = {
@@ -24,6 +42,7 @@ function TopMenu(){
     },[])
 
 
+
     return(
 
 
@@ -32,8 +51,11 @@ function TopMenu(){
     <div className="menu-container">
 
         <div className="option-container">
+            { user ? <button onClick={handleLogout}>로그아웃</button> : (
+            <>
         <Link to="/signup">회원가입</Link>
         <Link to="/login">로그인</Link>
+            </> )}
         <a href="#">주문조회</a>
         <a href="#">찜한상품</a>
         <a href="#">고객지원</a>
