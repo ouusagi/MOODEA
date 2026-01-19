8c7ecb1df3ceef74d2fc2fd0e9741530cc6fa8e0
ouusagi <xmflzhqxj123@naver.com>
2026-01-20 02:33:17 +0900

    MOODÉA - Skeleton UI 수정

--- src/components/Recycling/CategoryBarSkeleton.jsx
+++ src/components/Recycling/CategoryBarSkeleton.jsx
@@ -3,14 +3,14 @@ import "react-loading-skeleton/dist/skeleton.css";
 
 function CategoryBarSkeleton({ count }) {
   return (
-    <div className="CategoryBar-box">
+    <>
       {Array.from({ length: count }).map((_, i) => (
         <div className='CategoryBar-item' key={i}>
-        <Skeleton height={100} width="100%" borderRadius="9999px"/>
-        <Skeleton height={14} width="100%"/>
+        <Skeleton height={100} width="100%" borderRadius="9999px" />
+        <Skeleton height={10} width="100%"/>
         </div>
       ))}
-    </div>
+    </>
   );
 }
 
